43d8d6765241169134da4ef5e9b8f3e407843276
GoodDonkey <president20500@gmail.com>
2021-09-25 11:07:39 +0900

    upload

--- route.py
+++ route.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from flask_cors import CORS
+from werkzeug.utils import secure_filename
 import json
 
 app = Flask(__name__)
@@ -7,6 +8,26 @@ app = Flask(__name__)
     즉, 동일 출처가 아닐 때 에러가 발생하는데, 이를 해결해주는 flask_cors 를 사용한다.
     https://webisfree.com/2020-01-01/python-flask%EC%97%90%EC%84%9C-cors-cross-origin-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0 """
 CORS(app)
+
+
+@app.route("/", methods=['GET'])
+def home():
+    return render_template('upload.html')
+
+
+@app.route("/result", methods=['GET'])
+def result():
+    return render_template('index.html')
+
+
+@app.route("/upload", methods=['GET', 'POST'])
+def upload():
+    if request.method == "POST":
+        f = request.files['file']  # input 태그의 name 을 받음.
+        f.save(secure_filename(f.filename))
+        return redirect(url_for('result'))
+    else:
+        return '실패'
 
 
 @app.route("/json_data", methods=['GET', 'POST'])
