5f84ea3c0a12312b2e1852b260a7618436a83500
mechaniclib <ymk092792@gmail.com>
2021-10-14 19:15:53 +0900

    'charts'

--- static/js/script.js
+++ static/js/script.js
@@ -13,7 +13,11 @@ new Chart(document.getElementById("bar-chart"), {
       ]
     },
     options: {
-      legend: { display: false },
+      responsive: false,
+      legend: {
+        position: 'top',
+        display: false 
+      },
       title: {
         display: true,
         text: 'your sentiment statement'
@@ -35,6 +39,33 @@ new Chart(document.getElementById("doughnut-chart"), {
       ]
     },
     options: {
+      responsive: false,
+      title: {
+        display: true,
+        text: 'your sentiment statement'
+      }
+    }
+});
+
+// rader chart
+new Chart(document.getElementById("radar-chart"), {
+    type: 'radar',
+    data: {
+      labels: ["Positive", "Neutral", "Negative"],
+      datasets: [
+        {
+          label: "Sentiment",
+          fill: true,
+          backgroundColor: "rgba(179,181,198,0.2)",
+          borderColor: "rgba(179,181,198,1)",
+          pointBorderColor: "#fff",
+          pointBackgroundColor: "rgba(179,181,198,1)",
+          data: [0.3,0.4,0.3]
+        }
+      ]
+    },
+    options: {
+      responsive:false,
       title: {
         display: true,
         text: 'your sentiment statement'
