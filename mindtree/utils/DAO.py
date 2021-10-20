21f156dd5b8419e4bc184c1d410e1d07a5cee380
youngmin <ymk092792@gmail.com>
2021-10-20 10:08:43 +0900

    'daostart'

--- mindtree/utils/DAO.py
+++ mindtree/utils/DAO.py
@@ -1,16 +1,30 @@
 
-
 class User:
-
-    def sign_up(self):
+    def createUser():
+        pass
+    def searchUser():
+        pass
+    def updateUser():
+        pass
+    def deleteUser():
         pass
 
-
-
-
-
-class PostDAO:
+class Post:
+    def writePost():
+        pass
+    def searchPost():
+        pass
+    def updatePost():
+        pass
+    def 
     """ SQL Alchemy가 쿼리를 할 수 있도록 해주기 때문에 구현할 필요가 없음. """
+
+
+
+
+
+
+
     def __init__(self):
         pass
 
