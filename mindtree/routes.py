e66c2689b07f9f0aabe980299e822f0b045a4d49
kimself <ksy7678@gmail.com>
2021-10-26 13:28:49 +0900

    Add a diary image path to the analysis page

--- mindtree/routes.py
+++ mindtree/routes.py
@@ -104,12 +104,12 @@ def get_word_cloud_file(post_id):
     print("[get_word_cloud_file] word_cloud_file_name: ", word_cloud_file_name)
     return send_from_directory(path.get_user_media_path(post_id), word_cloud_file_name)
 
-# 기능: analysis page에 업로드 이미지 불러오기 위한 route
+# 기능: analysis page에 일기 이미지 불러오기 위한 route
 # 입력: post_id와 업로드한 일기 이미지 파일 이름을 input
-# 출력: 
-# 버전/일시: 2021.10.26 추가
+# 출력: post_id와 일기 이미지 파일 이름을 user_media_path 경로에 추가하여 출력
+# 버전/일시: ver 0.x/2021.10.26 추가
 # 개발자: 김수연
-@app.route("/results/<path:post_id>", methods=['GET'])
+@app.route("/results/diary_img/<path:post_id>", methods=['GET'])
 def get_upload_img(post_id):
     """ 
     일기 이미지 파일을 불러와서 analysis 웹 페이지에 업로드한 일기 이미지 전송
